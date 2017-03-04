b83ab1c7fca16abb221e7488b70efacef0eb0548
bwittman <bwittman@gmail.com>
2017-03-04 12:41:13 -0500

    Updated icons and added syntax highlighting for .meta files.

--- Shadow Plugin/src/shadow/plugin/editor/ShadowReconcilingStrategy.java
+++ Shadow Plugin/src/shadow/plugin/editor/ShadowReconcilingStrategy.java
@@ -2,7 +2,6 @@ package shadow.plugin.editor;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.eclipse.core.runtime.IProgressMonitor;
